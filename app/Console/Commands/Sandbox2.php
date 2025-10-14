43835488118f7a185251b56253ff270849897f93
David <david.soler@grupodesa.es>
2025-10-14 15:23:43 +0200

    - Ejercicio 1 de POO resuelto, (Herencia básica, Vehiculo-Coche-Moto) sobreescribiendo el método y utilizando parent::__construct()

--- app/Console/Commands/Sandbox2.php
+++ app/Console/Commands/Sandbox2.php
@@ -5,36 +5,50 @@ use Exception;
 use Illuminate\Console\Command;
 
 
-class Vehiculo {
+class Vehiculo
+{
     public string $marca;
     public string $modelo;
-
-    class Coche extends Vehiculo {
-        public int $numPuertas;
-    }
-    class Motos extends Vehiculo {
-        public int $cilindrada;
-    }
 
     public function __construct(string $marca, string $modelo)
     {
         $this->marca = $marca;
         $this->modelo = $modelo;
-        $this->numPuertas =$numPuertas;
-        $this->cilindrada =$cilindrada;
     }
-
     public function arrancar()
     {
-        print_r("El vehículo" . " " . $this->marca . " " .$this->modelo . " " ."está arrancando...");
-
+        print_r("El vehículo" . " " . $this->marca . " " .$this->modelo . " " ."está arrancando..." . PHP_EOL);
     }
 }
+    class Coche extends Vehiculo {
+        public int $numPuertas;
+
+        public function __construct(string $marca, string $modelo, int $numPuertas){
+            parent::__construct($marca, $modelo);
+            $this->numPuertas = $numPuertas;
+    }
+    public function arrancar(){
+        print_r("El coche" . " " . $this->marca . " " .$this->modelo . " " . "con numero de puertas " . $this->numPuertas ." está arrancando..." . PHP_EOL);
+    }
+    }
+    class Motos extends Vehiculo {
+        public int $cilindrada;
+        public function __construct(string $marca, string $modelo, int $cilindrada){
+            parent::__construct($marca, $modelo);
+            $this->cilindrada = $cilindrada;
+        }
+        public function arrancar(){
+            print_r("La moto" . " " . $this->marca . " " .$this->modelo . " " . "con cilindrada " . $this->cilindrada ." está arrancando..." . PHP_EOL);
+        }
+    }
+
+
+
 $miVehiculo = new Vehiculo("Skoda", "Octavia");
 $miVehiculo->arrancar();
 $miCoche = new Coche("Seat", "Ibiza", 4);
 $miCoche->arrancar();
-$miMoto = new Motos("Ducati", "Panigale", 1000);
+$miMoto = new Motos("Ducati", "Panigale", 700);
 $miMoto->arrancar();
 
 die;
