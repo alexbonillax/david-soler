899c3ffc98bebab9d46e6ff72d445adab26c962f
David <david.soler@grupodesa.es>
2025-11-19 09:10:18 +0100

    Inicio curso particular, CertiDevs PHP OOP
    Actividad 1-2-3

--- app/Console/Commands/CertiDevsPhpPoo.php
+++ app/Console/Commands/CertiDevsPhpPoo.php
@@ -18,3 +18,50 @@ class Coche{
 }
 $coche = new Coche("Seat", "Ibiza");
 $coche->imprimirInformacion();
+
+/////////////////////////////
+//ACT2: RETO CLASE LIBRO
+class Libro
+{
+    public string $titulo;
+
+    public function __construct(string $titulo)
+    {
+        $this->titulo = $titulo;
+    }
+
+    public function mostrarTitulo()
+    {
+        echo $this->titulo;
+    }
+}
+
+$libro = new Libro("El silencio de los corderos");
+$libro->mostrarTitulo();
+
+/////////////////////////////
+//ACT3: RETO CONSTRUCTOR
+
+class Product
+{
+    public string $name;
+    public float $price;
+    public int $stock;
+
+    public function __construct(string $name, float $price, int $stock)
+    {
+        $this->name = $name;
+        $this->price = $price;
+        $this->stock = $stock;
+    }
+
+    public function imprimirInformacion()
+    {
+        echo("Name:" . $this->name);
+        echo("Price" . $this->price);
+        echo("Stock" . $this->stock);
+    }
+}
+
+$product = new Product("Teclado mecánico", 79.99, 10);
+$product->imprimirInformacion();
